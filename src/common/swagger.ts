a19a8b6a4279c757fc250f10e0328ccced8bfe9a
Mick <mhearne@redhat.com>
2019-10-02 17:37:43 +0100

    initial commit

---
+++ src/common/swagger.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import middleware from 'swagger-express-middleware';
+import errorHandler from '../api/middlewares/error.handler';
+// eslint-disable-next-line no-unused-vars
+import { Application } from 'express';
+
+export default function(app: Application, routes: (app: Application) => void) {
+  middleware(path.join(__dirname, 'api.yml'), app, (err, middleware) => {
+    if (err) console.log(err);
+    // Enable Express' case-sensitive and strict options
+    // (so "/entities", "/Entities", and "/Entities/" are all different)
+    app.enable('case sensitive routing');
+    app.enable('strict routing');
+
+    app.use(middleware.metadata());
+    app.use(middleware.files(app, {
+      apiPath: process.env.SWAGGER_API_SPEC
+    }));
+
+    app.use(middleware.parseRequest({
+      // Configure the cookie parser to use secure cookies
+      cookie: {
+        secret: process.env.SESSION_SECRET
+      },
+      // Don't allow JSON content over 100kb (default is 1mb)
+      json: {
+        limit: process.env.REQUEST_LIMIT
+      }
+    }));
+
+    // These two middleware don't have any options (yet)
+    app.use(
+      middleware.CORS(),
+      middleware.validateRequest());
+
+    routes(app);
+
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    app.use(errorHandler);
+  });
+}
---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Application } from 'express';
+import examplesRouter from './api/controllers/examples/router'
+export default function routes(app: Application): void {
+  app.use('/api/v1/examples', examplesRouter);
+
+  // Add other routes here
+
+  // app.use('/api/v1/examples', examplesRouter);
+  // app.use('/api/v1/examples', examplesRouter);
+  // app.use('/api/v1/examples', examplesRouter);
+  // app.use('/api/v1/examples', examplesRouter);
+};
