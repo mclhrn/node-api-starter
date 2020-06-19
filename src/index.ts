df985240cb80b2e6c1d727677becc482cc990547
Mick <mhearne@redhat.com>
2020-06-19 15:48:11 +0100

    tidy

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import './lib/env'
 import Server from './lib/server'
 import routes from './routes'
 
-const port = parseInt(process.env.PORT)
 export default new Server()
   .router(routes)
   .connectDb(process.env.MONGO_CONN_STRING)
-  .listen(port)
+  .listen(process.env.PORT)
