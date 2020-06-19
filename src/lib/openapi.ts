ec315efc0fae6651da3b6d977f7a69825322b2b4
Mick <mhearne@redhat.com>
2020-06-19 11:52:59 +0100

    jkdsgfhjk

--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -7,13 +7,13 @@ import { serve, setup } from 'swagger-ui-express'
 export default function openapi(app, routes) {
   const apiSpec = path.join(__dirname, 'api.yml');
   app.use(`${process.env.OPENAPI_PATH_VERSION}/api-docs`, express.static(apiSpec));
-  // new OpenApiValidator({ apiSpec }).install(app);
+  new OpenApiValidator({ apiSpec }).install(app);
 
 
   console.log('**********')
   console.log(apiSpec)
 
-  app.use(`${process.env.OPENAPI_PATH_VERSION}/api-docs`, serve, setup(apiSpec));
+  // app.use(`${process.env.OPENAPI_PATH_VERSION}/api-docs`, serve, setup(apiSpec));
 
   routes(app);
 
